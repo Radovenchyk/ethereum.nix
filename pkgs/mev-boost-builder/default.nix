89d88b7b159f8d16f3cf59f4e9601febf3f20fb7
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2024-02-16 21:07:20 +0000

    mev-boost-builder: 1.11.5-0.3.0 -> 1.13.12.4844.dev1

--- pkgs/mev-boost-builder/default.nix
+++ pkgs/mev-boost-builder/default.nix
@@ -6,16 +6,16 @@
 }:
 buildGoModule rec {
   pname = "builder";
-  version = "1.11.5-0.3.0";
+  version = "1.13.12.4844.dev1";
 
   src = fetchFromGitHub {
     owner = "flashbots";
     repo = "${pname}";
     rev = "v${version}";
-    hash = "sha256-5c0a+/dl1/B1PdEMsrUGfjgCE/zZtm6mBNHKBFTyoGc=";
+    hash = "sha256-nqntWcaeWFGjacKufRFFqxlG/rfn20t7k6Pk5vptFkY=";
   };
 
-  vendorHash = "sha256-PwsJjcgPXQuOtXDr4NjF7IEk+nrhDBfEnQQyyBFFSjE=";
+  vendorHash = "sha256-tCksME1tmsRQcrOEiRm5eZNljmIvDvFBJCJmU/e8sW8=";
 
   buildInputs = [blst];
 
